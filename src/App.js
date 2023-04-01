c6e2398aaaa27d165c7fd375e300f841bd52aef0
belmontprogramador <127756640+belmontprogramador@users.noreply.github.com>
2023-04-01 17:38:11 -0300

    primiera salvada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Video from "./pages/Video";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__videos">
+        <Video
+          likes={111}
+          messages={222}
+          shares={333}
+          name="@Feipe Belmont"
+          description="maior goleiro do brasil"
+          music="musica epica fim do mundo"
+          url = " https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+        likes={444} 
+        messages={555} 
+        shares={666} 
+        name="@Jorge Luiz"
+          description="ta doidão mano"
+          music="Calp your hand"
+          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
+        />
+      </div>
     </div>
   );
 }
